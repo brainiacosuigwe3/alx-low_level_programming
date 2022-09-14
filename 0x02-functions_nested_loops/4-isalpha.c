ad38c0e813ceea83e37f8f8251504326b55dce4f
brainiacosuigwe3 <112703974+brainiacosuigwe3@users.noreply.github.com>
2022-09-14 01:40:43 +0100

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _isalpha - Check Holberton
+ * _isalpha - Check main
  * @c: An input character
  * Description: function returns 1 if the character is a
  * letter, lowercase or uppercase.
@@ -9,16 +9,16 @@
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
+char lower, upper;
+int isletter = 0;
 
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if (c == lower || c == upper)
-				isletter = 1;
-		}
-	}
-	return (isletter);
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+	if (c == lower || c == upper)
+		isletter = 1;
+}
+}
+return (isletter);
 }
