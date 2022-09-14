8a0ab606047aaab77c1710dde9eb07cec924fbb9
brainiacosuigwe3 <112703974+brainiacosuigwe3@users.noreply.github.com>
2022-09-14 01:33:11 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 #include "main.h"
- /**
- * main - Check description
- *
- * Description: It prints the word _putchar, followed by a new line.
- * Return: Always 0 (Success)
- */
+/**
+*main - It prints the word _putchar, followed by a new line.
+*
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
 char word[8] = "_putchar";
