db426e2231730ef07d23c64aeff7f51a0900c4d1
brainiacosuigwe3 <112703974+brainiacosuigwe3@users.noreply.github.com>
2022-09-14 01:19:41 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,16 @@
 #include <unistd.h>
-#include "main.h" 
- /** 
+#include "main.h"
+ /**
  *main-Prints putchar
- * Return: Always 0 (Success) 
- */ 
- int main(void)
+ *Return: Always 0 (Success)
+ */
+int main(void)
 {
-	char word[8] = "_putchar";
-	int i;
+char word[8] = "_putchar";
+int i;
 
-	for (i = 0; i < 8; i++)
-		_putchar(word[i]);
-	_putchar('\n');
-
-	return (0);
+for (i = 0; i < 8; i++)
+	_putchar(word[i]);
+_putchar('\n');
+return (0);
 }
