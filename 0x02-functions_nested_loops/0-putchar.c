4fc7f841da4005a407bd95d13a1f5adaaf5be376
brainiacosuigwe3 <112703974+brainiacosuigwe3@users.noreply.github.com>
2022-09-14 01:14:38 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include "main.h" 
  /** 
  *main-Prints putchar
@@ -6,11 +7,11 @@
  */ 
  int main(void) 
  { 
-         _putchar('_'); 
-         _putchar('p'); 
-         _putchar('u'); 
-         _putchar('t'); 
-         _putchar('c'); 
+   _putchar('_'); 
+   _putchar('p'); 
+   _putchar('u'); 
+   _putchar('t'); 
+   _putchar('c'); 
          _putchar('h'); 
          _putchar('a'); 
          _putchar('r'); 
